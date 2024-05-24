5c193a1b46a5dfdcc71f19f8b1ceb5125ddde086
Zedro <45104292+PedroZappa@users.noreply.github.com>
2024-05-24 17:24:55 +0100

    thread_sum/main.c : implmented mutexes

--- thread_sum/main.c
+++ thread_sum/main.c
@@ -6,12 +6,17 @@
 #include <unistd.h>
 #include <pthread.h>
 
+typedef pthread_mutex_t	t_mutex;
+
 typedef struct s_data
 {
-	int n_threads;
-	int n_numbers;
-	int n_per_thread;
-	int *idx;
+	int		n_threads;
+	int		n_numbers;
+	int		n_per_thread;
+	int		sum;
+	int		*idx;
+	t_mutex mtx_sum;
+	t_mutex mtx_printf;
 } t_data;
 
 void	*routine(void *arg);
@@ -42,6 +47,16 @@ int main(int argc, char **argv)
 	data->n_numbers = atoi(argv[2]);
 	data->n_per_thread = (data->n_numbers / data->n_threads);
 	list = gen_list(data->n_numbers);
+	if (pthread_mutex_init(&data->mtx_sum, NULL))
+	{
+		perror("Error: pthread_mutex_init");
+		exit(1);
+	}
+	if (pthread_mutex_init(&data->mtx_printf, NULL))
+	{
+		perror("Error: pthread_mutex_init");
+		exit(1);
+	}
 	for (i = 0; i < data->n_threads; i++)
 	{
 		data->idx = malloc(sizeof(int));
@@ -61,8 +76,7 @@ int main(int argc, char **argv)
 			perror("Error: pthread_join");
 			exit(1);
 		}
-		g_sum += *res->idx;
-		free(res->idx);
+		g_sum += res->sum;
 	}
 	printf("Global Sum: %d\n", g_sum);
 	free(data->idx);
@@ -79,10 +93,15 @@ void	*routine(void *ref)
 	t_data *data;
 	int sum = 0;
 	data = (t_data *)ref;
+	usleep(100);
+	pthread_mutex_lock(&data->mtx_sum);
 	for (int i = 0; i < data->n_per_thread; i++)
 		sum += *(list + *data->idx + i);
+	pthread_mutex_unlock(&data->mtx_sum);
+	pthread_mutex_lock(&data->mtx_printf);
 	printf("Thread Local Sum: %d\n", sum);
-	data->idx = &sum;
+	pthread_mutex_unlock(&data->mtx_printf);
+	data->sum = sum;
 	return (ref);
 }
 
